f40bc2a08d89f14b6eb5472b81cc762ad1c6d350
xiaoshuisheng <etaxiao@outlook.com>
2023-11-10 17:14:45 +0800

    [MOD] update script

--- webui.sh
+++ webui.sh
@@ -4,6 +4,27 @@
 # change the variables in webui-user.sh instead #
 #################################################
 
+service="webui"
+pids=$(ps -ef | grep "$service" | grep -v grep | awk '{print $2}')
+
+if [ -n "$pids" ]; then
+    for pid in $pids; do
+        kill -9 "$pid"
+    done
+fi
+
+# 查找 7860 端口的进程 ID
+pid=$(lsof -t -i:7860)
+
+# 如果找到了进程 ID，则杀死进程
+if [ -n "$pid" ]; then
+  echo "Killing process $pid"
+  kill $pid
+else
+  echo "No process found on port 7860"
+fi
+
+
 
 
 use_venv=1
