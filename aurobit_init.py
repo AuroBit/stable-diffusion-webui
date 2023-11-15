f2e85e5dae546fffe46524d8ed0a87dfa4f67e44
xiaoshuisheng <etaxiao@outlook.com>
2023-11-15 10:50:46 +0800

    [MOD] udpate aurobit_init, support resource cache, support download from tx or s3

--- aurobit_init.py
+++ aurobit_init.py
@@ -1,46 +1,103 @@
 import urllib.request
 import os
+import argparse
+import shutil
 
 from git import repo
 
 
-# download models
-sd_models = [
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/beautifulRealistic_brav5.safetensors",
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/majicmixRealistic_v5.safetensors",
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/realisticVisionV51_v51VAE-inpainting.safetensors"
-]
-sd_path = 'models/Stable-diffusion'
-for model_file in sd_models:
-    base_name = os.path.basename(model_file)
-    dl_path = f'{sd_path}/{base_name}'
-    if not os.path.exists(dl_path):
-        print(f'Downloading model: {model_file}')
-        urllib.request.urlretrieve(model_file, dl_path)
+# parse arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-cache', default='yes', help="This is argument 1")
+parser.add_argument('-s3', default='yes', help="This is argument 2")
+
+args = parser.parse_args()
+cache_resource = True if args.cache == 'yes' else False
+use_s3 = True if args.s3 == 'yes' else False
+
+print(f"Cache resource: {cache_resource}")
+print(f"Use S3: {use_s3}")
+
+cache_dir = "/workspace/sd_resource"
+
+resource_path = {
+    "tencent": {
+        "sd_models": [
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/beautifulRealistic_brav5.safetensors",
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/majicmixRealistic_v5.safetensors",
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_models/realisticVisionV51_v51VAE-inpainting.safetensors"
+        ],
+        "sd_lora": ["https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_lora/meiyan_V1.safetensors"],
+        "controlnet_models": [
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/controlnet_models/control_v11f1p_sd15_depth.pth",
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/controlnet_models/control_v11p_sd15_canny.pth"
+        ],
+        "ad_ext_models": [
+            "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_other/sow_pyramid_a5_e3d2_remapped.pth"
+        ]
+    },
+    
+    "s3": {
+        "sd_models": [
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/sd_models/beautifulRealistic_brav5.safetensors",
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/sd_models/majicmixRealistic_v5.safetensors",
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/sd_models/realisticVisionV51_v51VAE-inpainting.safetensors"
+        ],
+        "sd_lora": ["https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/lora/meiyan_V1.safetensors"],
+        "controlnet_models": [
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/controlnet/control_v11f1p_sd15_depth.pth",
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/controlnet/control_v11p_sd15_canny.pth"
+        ],
+        "ad_ext_models": [
+            "https://aurobit-s3-01.s3.ap-northeast-1.amazonaws.com/common_resource/sd/sd_others/sow_pyramid_a5_e3d2_remapped.pth"
+        ]
+    },
+}
+
+
+def ab_download_resource(url, file_dir):
+    base_name = os.path.basename(url)
+    
+    # make target dir
+    if not os.path.exists(file_dir):
+        os.makedirs(file_dir)
+    
+    # cache resource
+    if cache_resource:
+        # download to cache dir
+        file_cache_dir = f'{cache_dir}/{file_dir}'
+        file_cache_path = f'{file_cache_dir}/{base_name}'
+        if not os.path.exists(file_cache_dir):
+            os.makedirs(file_cache_dir)
+        if not os.path.exists(file_cache_path):
+            urllib.request.urlretrieve(url, file_cache_path)
+            print(f'[Download to cache] {url} -> {file_cache_path}')
+        else:
+            print(f'[Download to cache] file exists: {file_cache_path}')
+            
+        # copy from cache
+        file_path = f'{file_dir}/{base_name}'
+        if not os.path.exists(file_path):
+            shutil.copy(file_cache_path, file_path)
+            print(f'[Copy from cache] {file_cache_path} -> {file_path}')
+        else:
+            print(f'[Copy from cache] file exists: {file_path}')
+        
+    # download directly
     else:
-        print(f'Model exist: {model_file}')
-    print(f'Download model DONE: {model_file}')
+        file_path = f'{file_dir}/{base_name}'
+        if not os.path.exists(file_path):
+            urllib.request.urlretrieve(url, file_path)
+            print(f'[Download directly] {url} -> {file_path}')
+        else:
+            print(f'[Download directly] file exists: {file_path}')
+            
 
 
-# download loras
-sd_lora = [
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_lora/meiyan_V1.safetensors"
-]
-sd_path = 'models/Lora'
-if not os.path.exists(sd_path):
-    os.makedirs(sd_path)
-for model_file in sd_lora:
-    base_name = os.path.basename(model_file)
-    dl_path = f'{sd_path}/{base_name}'
-    if not os.path.exists(dl_path):
-        print(f'Downloading lora: {model_file}')
-        urllib.request.urlretrieve(model_file, dl_path)
-    else:
-        print(f'LORA exist: {model_file}')
-    print(f'Download lora DONE: {model_file}')
 
+# ==================== prepare extensions ============================
 
-# extensions
 ext_git_path = {
     "sd-webui-controlnet": "https://github.com/Mikubill/sd-webui-controlnet",      # controlnet
     # "adetailer": "https://github.com/Bing-su/adetailer.git",             # adtailer
@@ -60,41 +117,44 @@ for repo_name, rep_path in ext_git_path.items():
         print(e.args)
     print(f'Clone repo DONE: {rep_path}')
 
+    
+    
+
+# ====================== prepare resource =================================
+
+all_resource = resource_path["s3"] if use_s3 else resource_path["tencent"]
+sd_path = 'models/Stable-diffusion'
+sd_lora_path = 'models/Lora'
+control_net_dir = f'{ext_path}/sd-webui-controlnet/models'
+ad_rel_model_dir = f"models/adetailer"
+
+
+# download models
+print(f'************ Download SD models ********************')
+sd_models = all_resource["sd_models"]
+for model_file in sd_models:
+    ab_download_resource(model_file, sd_path)
+
+
+# download loras
+print(f'************* Download Lora ******************')
+sd_lora = all_resource["sd_lora"]
+for model_file in sd_lora:
+    ab_download_resource(model_file, sd_lora_path)
+
 
 # download control net model
-control_net_dir = f'{ext_path}/sd-webui-controlnet'
-controlnet_models = [
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/controlnet_models/control_v11f1p_sd15_depth.pth",
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/controlnet_models/control_v11p_sd15_canny.pth"
-]
-cn_model_dir = f'{control_net_dir}/models'
+print(f'************* Download controlnet models ******************')
+controlnet_models = all_resource["controlnet_models"]
 if os.path.exists(control_net_dir):
     for model_file in controlnet_models:
-        base_name = os.path.basename(model_file)
-        local_path = f'{cn_model_dir}/{base_name}'
-        if not os.path.exists(local_path):
-            print(f'Downloading controlnet: {model_file}')
-            urllib.request.urlretrieve(model_file, local_path)
-        else:
-            print(f'Controlnet exist: {model_file}')
-        print(f'Download controlnet DONE: {model_file}')
+        ab_download_resource(model_file, control_net_dir)
 
 # download ad-relative models
-ad_rel_models = [
-    "https://aiyo-1319341997.cos.ap-nanjing.myqcloud.com/common_resource/sd_other/sow_pyramid_a5_e3d2_remapped.pth"
-]
-ad_rel_model_dir = f"models/adetailer"
-if not os.path.exists(ad_rel_model_dir):
-    os.makedirs(ad_rel_model_dir)
+print(f'************* Download ad-relative models ******************')
+ad_rel_models = all_resource["ad_ext_models"]
 for md_url in ad_rel_models:
-    md_name = os.path.basename(md_url)
-    local_path = f"{ad_rel_model_dir}/{md_name}"
-    if not os.path.exists(local_path):
-        print(f'Downloading ad-relative models: {md_url}')
-        urllib.request.urlretrieve(md_url, local_path)
-    else:
-        print(f'Ad-relative model exist: {md_url}')
-        print(f'Download ad-relative model  DONE: {md_url}')
+    ab_download_resource(md_url, ad_rel_model_dir)
             
         
     
--- webui.sh
+++ webui.sh
@@ -221,7 +221,7 @@ fi
 
 # Aurobit custom init
 pip install GitPython
-"${python_cmd}" aurobit_init.py
+"${python_cmd}" aurobit_init.py -cache yes -s3 yes
 
 
 # Try using TCMalloc on Linux
